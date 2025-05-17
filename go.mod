6504f962cfa1916e2ab7b29e0b5e8fbffa2d64b6
Nicholas Kralles <kralles-nick@n-ask.com>
2025-05-17 13:58:15 -0400

    adding pg_fancylog trace

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.wg.nask.world/nask/pg_fancylog.git
+
+go 1.24.2
---
+++ log.go
@@ -0,0 +1,82 @@
+package pg_fancylog
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/n-ask/fancylog"
+	"regexp"
+	"time"
+)
+
+var (
+	re_leadclose_whtsp = regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
+	re_inside_whtsp    = regexp.MustCompile(`[\s\p{Zs}]{2,}`)
+)
+
+type LoggingQueryTracer struct {
+	logger fancylog.FancyLogger
+}
+
+func NewLoggingQueryTracer(logger fancylog.FancyLogger) *LoggingQueryTracer {
+	return &LoggingQueryTracer{logger: logger}
+}
+
+type sqlTracer struct {
+	data    *pgx.TraceQueryStartData
+	startTs time.Time
+}
+
+func (t *sqlTracer) GetSQL() string {
+	if t.data != nil {
+		final := re_leadclose_whtsp.ReplaceAllString(t.data.SQL, "")
+		final = re_inside_whtsp.ReplaceAllString(final, " ")
+		return final
+	}
+	return ""
+}
+
+func (l *LoggingQueryTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
+	return context.WithValue(ctx, "fancylog", &sqlTracer{data: &data, startTs: time.Now()})
+}
+
+func (l *LoggingQueryTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) {
+	start, ok := ctx.Value("fancylog").(*sqlTracer)
+	finish := time.Now()
+	args := make(map[string]any)
+	if ok {
+		args["duration"] = finish.Sub(start.startTs).String()
+		args["args"] = start.data.Args
+		args["sql"] = start.GetSQL()
+		if data.Err != nil {
+			args["error"] = data.Err.Error()
+			l.logger.ErrorMap(args)
+		} else {
+			if data.CommandTag.Delete() || data.CommandTag.Insert() || data.CommandTag.Insert() {
+				args["rowAffected"] = data.CommandTag.RowsAffected()
+			} else {
+				args["rowsReturned"] = data.CommandTag.RowsAffected()
+			}
+			l.logger.DebugMap(args)
+		}
+	}
+}
+
+func NewPoolWithTrace(ctx context.Context, log fancylog.FancyLogger, databaseURL string) (*pgxpool.Pool, error) {
+
+	connConfig, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		return nil, err
+	}
+	connConfig.ConnConfig.Tracer = NewLoggingQueryTracer(log)
+
+	pool, err := pgxpool.NewWithConfig(ctx, connConfig)
+	if err != nil {
+		return nil, err
+	}
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+	return pool, nil
+}
